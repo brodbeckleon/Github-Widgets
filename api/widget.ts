9b9fe1c5e50aa59a8cbdf5abc2d2af54dd312665
Léon Brodbeck <brodbeckleon@gmail.com>
2025-10-24 10:25:31 +0900

    initial commit

---
+++ api/widget.ts
@@ -0,0 +1,17 @@
+import { createCanvas } from "canvas";
+
+export default function handler(req, res) {
+    const { username = "Anonymous" } = req.query;
+    const canvas = createCanvas(400, 120);
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0, 0, 400, 120);
+
+    ctx.fillStyle = "#0f0";
+    ctx.font = "bold 24px Sans";
+    ctx.fillText(`Hello, ${username}!`, 40, 70);
+
+    res.setHeader("Content-Type", "image/png");
+    canvas.pngStream().pipe(res);
+}
